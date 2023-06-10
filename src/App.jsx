e0e0961c989c59451303f3152aeb16e20d8c0f15
jose <jramon_ibarra@hotmail.com>
2023-06-09 23:02:59 -0700

    Se agregaron los estilos CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import axios from "axios"
+import "./App.css"
+import Cripto from "./Cripto"
 
+/*
+  PETICIONES A UNA API DE TIPO GET
+
+    * Lo que fetch() realiza es una peticion al link que le coloquemos
+    * .then() recibe la respuesta y ejecuta una accion con esa respuesta, en este caso la convertira en una de tipo json
+    * El segundo .then() recibe la data de la accion anterior y ejecuta otra funcion
+    * Por ultimo .catch() se ejecutara en caso de que la peticion falle y nos mostrara la accion que le indiquemos, en este caso un console.error()
+
+*/
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // El useState() va almacenar la informacion que se encuentre en la data que nos regresa el .then() del fetch()
+
+  // El array vacio en useState([]) es para evitar que nos marque un error ya que la informacion tardaría en llegar a la funcion de criptos.map()
+
+  // La "const API_URL" importa el url de la API para poderlo cambiar desde el archivo .env.local en caso de ser necesario
+
+  const API_URL = import.meta.env.VITE_API_URL
+
+  const [criptos, setCriptos] = useState()
+  
+
+  // La libreria de axios nos sirve para sustituir fetch y cumple la misma funcion, la ventaja es axios realiza la conversion de la respuesta a json de manera automatica.
+
+  // En cualquiera de los dos casos debemos de hacer un console.log primero para saber que es lo que data nos está devolviendo y cuandos ".data" tendremos que poner
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+    // fetch(`${API_URL}assets`)
+      // .then((resp) => resp.json())
+      .then((data) => {
+        // console.log(data)
+        setCriptos(data.data.data)
+      })
+      .catch(() => {
+        console.error("La peticion falló")
+      })
+  }, [])
+
+  // Este if lo colocamos para quitar el array vacio de useState([]) y dar una solucion mas optima al problema
+
+  if (!criptos) return <span>Cargando...</span>
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    // criptos.map() va a crear un nuevo array tomando la informacion que esta almacenada en useState y con la siguiente funcion la va a retornar a manera de texto tomando solo los parametros que le estamos indicando, en este caso "name" y "priceUsd".
+
+    // Para que react pueda diferenciar entre cada <li></li> en caso de haber mas de una, necesitamos otorgarle un valor a la propiedad key={} para cada una
+
+    <div className="app-container">
+      <h1>Lista de criptomonedas</h1>
+      <div className="cripto-container">
+        { 
+          criptos.map(({id, name, priceUsd, symbol, changePercent24Hr}) => (
+            <Cripto 
+              key={id} 
+              name={name} 
+              priceUsd={priceUsd} 
+              symbol={symbol} 
+              changePercent24Hr={changePercent24Hr} 
+            /> 
+          ))
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
