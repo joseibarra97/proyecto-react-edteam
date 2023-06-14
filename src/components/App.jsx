0c7679db30bc2a3bf5003ca27f3b2b4077e6b1ae
jose <jramon_ibarra@hotmail.com>
2023-06-14 00:41:41 -0700

    Instalando dependencias de gh-pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Menu from "./menu/Menu"
 import "./App.css"
-import Footer from "./PieDePagina/Footer"
 
 // En este componente App colocamos el Menu y el componente Outlet indicando que toda pagina va a tener un Menu y Outlet le va a decir al grupo de rutas que tenemos en main.jsx que deberan usar esta estructura la cual les indica que siempre van a tener un menu y depues van a reemplazar el Outlet por el componente que estan referenciando en cada una de sus rutas
 
@@ -10,7 +9,6 @@ const App = () => {
         <div className="app-container">
             <Menu />
             <Outlet />
-            <Footer />
         </div>
     )
 }
